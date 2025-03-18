be1f43dd1e52885edbb022dfa96813e3acea0e62
Orlando <lalomax365@gmail.com>
2025-03-18 12:54:00 +0000

    Root message

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 4000;
-
+app.get('/', (req, res) => {
+    res.status(200).json('This is my new backend');
+});
 app.get('/home', (req, res) => {
     res.status(200).json('Welcome, your app is working well');
 });
